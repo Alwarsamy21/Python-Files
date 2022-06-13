256209845456845d31a1f3d33d35e2f038ee585b
Alwarsamy21 <107179075+Alwarsamy21@users.noreply.github.com>
2022-06-13 19:10:23 +0530

    Add files via upload

---
+++ Basics Practice.py
@@ -0,0 +1,50 @@
+# Variables and Data types
+a = -1115
+b = 256.54
+c = 6j
+d = "Hello World"
+e = False
+
+# print() -> python standard function
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(a, b, c, d, e, 100, "hello world")
+
+
+#trio
+a = 50
+print(a)         #value
+print(type(a))   #data type
+print(id(a))     #memory adress
+
+a = False
+print(a)         
+print(type(a))   
+print(id(a))
+
+a = "Hello world"
+print(a)         
+print(type(a))   
+print(id(a))
+
+
+# type casting
+# its a method that changing from one data type to another data type
+
+a = float (10)
+print(a, type(a))
+
+b = int(10.88)
+print(b, type(b))
+
+c = str(10)
+print(c, type(c))
+
+d = int("12345")
+print(d, type(d))
+
+e = bool("123")
+print(e, type(e))
