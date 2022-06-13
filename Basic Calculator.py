fb938c63db06275977d0568725c28efea51242f6
Alwarsamy21 <107179075+Alwarsamy21@users.noreply.github.com>
2022-06-13 19:56:40 +0530

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,60 @@
+#Addition
+
+def addTwoNums(n1, n2): 
+ return n1 + n2
+
+a = input('Enter first number: \n')
+b = input('Enter second number: \n')
+
+a = int(a)
+b = int(b)
+
+
+print ("Addition",addTwoNums(a, b))
+
+
+#subtraction
+
+def subTwoNums(n1, n2): 
+ return n1 - n2
+
+a = input('Enter first number: \n')
+b = input('Enter second number: \n')
+
+a = int(a)
+b = int(b)
+
+
+print ("Subtraction",subTwoNums(a, b))
+
+
+#Multiplication
+
+def MultiplyTwoNums(n1, n2): 
+ return n1 * n2
+
+a = input('Enter first number: \n')
+b = input('Enter second number: \n')
+
+a = int(a)
+b = int(b)
+
+
+print ("Multiplication",MultiplyTwoNums(a, b))
+
+
+
+#Division
+
+def divideTwoNums(n1, n2): 
+ return n1 / n2
+
+a = input('Enter first number: \n')
+b = input('Enter second number: \n')
+
+a = int(a)
+b = int(b)
+
+
+print ("Division",divideTwoNums(a, b))
+
