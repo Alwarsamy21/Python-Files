f04bb4b198847ffc1b6468c7c9b3931d9eaf5068
Alwarsamy21 <107179075+Alwarsamy21@users.noreply.github.com>
2022-06-13 18:35:53 +0530

    Add files via upload

---
+++ Symbols Practice.py
@@ -0,0 +1,45 @@
+#Arithmetic operators
+
+print(15 + 10)
+print(15 - 10)
+print(15 * 10)
+print(15 ** 10)
+print(15 / 10)
+print(15 // 10)
+print(15 % 10)
+
+
+#Assignment operators
+
+a = 10
+print (a)
+
+a += 5
+print (a)
+
+a *= 2
+print (a)
+
+a *= a
+print (a)
+
+
+#comparison Operator
+
+print (10 == 10)
+print (10 != 10)
+print (10 >= 10)
+print (10 > 10)
+print (10 <= 10)
+print (10 < 10)
+
+
+#Logical operator
+
+print (10==10 and 4<6)
+print (10==10 and 4>6)
+print (10==10 or 4<6)
+print (not 10==10) 
+
+
+
