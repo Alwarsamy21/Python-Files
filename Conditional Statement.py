b554d6a06ad0d784195fe4c9e0ab0ea50b51362d
Alwarsamy21 <107179075+Alwarsamy21@users.noreply.github.com>
2022-06-14 19:04:50 +0530

    Add files via upload

---
+++ Conditional Statement.py
@@ -0,0 +1,67 @@
+# if condition
+
+a = 101
+if a>100:
+    print("a is bigger than 100")
+
+a = 10
+if a>100:
+    print("a is greater than 100")
+    print("hello world")
+
+
+#if + else
+a = 10
+if a>100:
+    print("a is greater than 100")
+    print("hello world")
+else:
+    print("a is smaller than 100")
+    print("code inside else condition")
+
+a = 1000
+if a>100:
+    print("a is greater than 100")
+    print("hello world")
+else:
+    print("a is smaller than 100")
+    print("code inside else condition")
+
+
+# if + elif + else
+
+a = 100
+if a>100:
+    print("a is bigger than 100")
+    print("code inside if condition")
+    
+elif a == 100:
+    print("a is 100")
+
+else:
+    print("a is smaller than 100")
+    print("code inside else condition")
+
+
+a = 500
+if a>100:
+    print("a is bigger than 100")
+    print("code inside if condition")
+    
+elif a == 100:
+    print("a is 100")
+
+else:
+    print("a is smaller than 100")
+    print("code inside else condition")
+
+
+# if + elif
+
+a = 100
+if a == 100:
+    print("a is 100")
+
+elif a == 200:
+    print("a is 200")
+
